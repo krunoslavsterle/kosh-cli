cb47430ad96dc2dce0f702f348c0445da819ab73
Krunoslav Sterle <krunoslavsterle@gmail.com>
2026-01-19 14:46:29 +0100

    feat: add version command

--- src/KoshCLI/Terminal/KoshConsole.cs
+++ src/KoshCLI/Terminal/KoshConsole.cs
@@ -10,7 +10,7 @@ public static class KoshConsole
         var prefix = $"[bold {color.ToMarkup()}]{service}[/]";
         var escaped = Markup.Escape(message);
 
-        AnsiConsole.MarkupLine($"[[{prefix}]]: {escaped}");
+        AnsiConsole.MarkupLine($"> [[{prefix}]]: {escaped}");
     }
 
     public static void WriteServiceErrorLog(string service, string message)
@@ -25,11 +25,11 @@ public static class KoshConsole
     public static void Empty() => AnsiConsole.MarkupLine("");
 
     public static void Info(string message) =>
-        AnsiConsole.MarkupLine($"[yellow]> [black on yellow] kosh [/] ! {message}[/]");
+        AnsiConsole.MarkupLine($"[yellow]> [black on yellow] kosh [/] {message}[/]");
 
     public static void Error(string message) =>
-        AnsiConsole.MarkupLine($"[red]> [black on red] kosh [/] ✗ {Markup.Escape(message)}[/]");
+        AnsiConsole.MarkupLine($"[red]> [black on red] kosh [/] {Markup.Escape(message)}[/]");
 
     public static void Success(string message) =>
-        AnsiConsole.MarkupLine($"[green]> [black on green] kosh [/] ✓ {message}[/]");
+        AnsiConsole.MarkupLine($"[green]> [black on green] kosh [/] {message}[/]");
 }
