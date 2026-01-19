de6c5c4f63209347e7bb79e247afdebd0f9342a6
Krunoslav <krunoslavsterle@gmail.com>
2026-01-19 19:22:14 +0100

    feat: add init command

--- src/KoshCLI/Program.cs
+++ src/KoshCLI/Program.cs
@@ -9,6 +9,10 @@ app.Configure(config =>
     config.SetApplicationName("kosh");
 
     config
+        .AddCommand<InitCommand>("init")
+        .WithDescription($"Creates {Constants.ConfigFile} file in the current directory");
+
+    config
         .AddCommand<StartCommand>("start")
         .WithDescription($"Starts all services defined in {Constants.ConfigFile}");
 
